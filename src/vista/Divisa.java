cc578d9fe2bd1891bae36c4e6afc899b1114c990
MichaelMarq <michael-marquez@outlook.com>
2023-09-25 15:19:29 -0500

    Creacion de la interfaz de ventana divisa

--- src/vista/Divisa.java
+++ src/vista/Divisa.java
@@ -1,15 +1,22 @@
 
 package vista;
 
+import java.awt.Color;
 import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 
 
 public class Divisa extends JFrame{
@@ -19,7 +26,13 @@ public class Divisa extends JFrame{
     private JMenuBar jBarraMenu;
     private JMenu jMenu;
     private JMenuItem jItemCalEstandar,jItemCalDivisa,jItemSalir;
-    
+    private JPanel panel;
+    private GridBagLayout design;
+    private GridBagConstraints c;
+    private JLabel jlDivisa1,jlDivisa2,jlCambio,jlCambio2;
+    private JComboBox comboDivisa,comboDivisa2;
+    private JButton btnC,btnBorrar,btn7,btn8,btn9,btn4,btn5,btn6,btn1,btn2,btn3,btn0,btnPunto;
+
     
     //Constructor
     
@@ -44,6 +57,15 @@ public class Divisa extends JFrame{
         agregarBarraMenu();
         agregarMenu();
         agregarMenuItem();
+        agregarPanel();
+        agregarDesign();
+        agregarEtiquetaDivisa();
+        agregarEtiquetaCambio();
+        agregarComboDivisa();
+        agregarEtiquetaDivisa2();
+        agregarEtiquetaCambio2();
+        agregarComboDivisa2();
+        agregarBoton();
     }
     
     //Inicializar ventana
@@ -86,9 +108,6 @@ public class Divisa extends JFrame{
          
     }
     
-    
-    
-    
     //Evento salir del item de la barra de menu
     private void eventoItemSalir(){
         
@@ -102,8 +121,6 @@ public class Divisa extends JFrame{
         jItemSalir.addActionListener(evento);
         
     }
-    
-    
     
     
     //Evento abrir la ventana de divisa
@@ -126,4 +143,353 @@ public class Divisa extends JFrame{
         
     }
     
+    //Agregar panel
+    private void agregarPanel(){
+        panel = new JPanel();
+        this.add(panel);
+    }
+    
+    //Agregar el diseño
+    private void agregarDesign(){
+        design = new GridBagLayout();
+        panel.setLayout(design);
+        c = new GridBagConstraints();
+    }
+    
+    //Agregar la etiqueta divisa 1
+    private void agregarEtiquetaDivisa(){
+        jlDivisa1 = new JLabel("$");
+        jlDivisa1.setFont(new Font("Arial", 0, 36));
+        jlDivisa1.setHorizontalAlignment(SwingConstants.RIGHT);
+        
+        
+        c.gridx = 0;
+        c.gridy = 0;
+        c.gridwidth = 1;
+        c.gridheight = 1;
+        c.fill = GridBagConstraints.BOTH;
+        
+        c.insets = new Insets(15, 15, 0, 5);
+        c.weightx = 0.5;
+        c.weighty = 0.5;
+        
+        
+        panel.add(jlDivisa1, c);
+    }
+    
+    //Agregar etiqueta cambio 1
+    private void agregarEtiquetaCambio(){
+        jlCambio = new JLabel("0");
+        jlCambio.setFont(new Font("Arial", 0, 36));
+        
+        c.gridx = 1;
+        c.gridy = 0;
+        c.gridwidth = 2;
+        c.gridheight = 1;
+        
+        c.fill = GridBagConstraints.BOTH;
+        c.anchor = GridBagConstraints.CENTER;
+        c.insets = new Insets(15, 0, 0, 0);
+        c.weightx = 0.5;
+        c.weighty = 0.5;
+        
+        panel.add(jlCambio, c);
+    }
+    
+    //Agregar combo divisa 1
+    private void agregarComboDivisa(){
+        comboDivisa = new JComboBox();
+        comboDivisa.setFont(new Font("Arial", 0, 14));
+        
+        
+        c.gridx = 0;
+        c.gridy = 1;
+        c.gridwidth = 3;
+        c.gridheight = 1;
+        
+        c.fill = GridBagConstraints.BOTH;
+        c.anchor = GridBagConstraints.CENTER;
+        c.insets = new Insets(0, 10, 0, 10);
+        c.weightx = 0.5;
+        c.weighty = 0.5;
+        
+        
+        panel.add(comboDivisa, c);
+    }
+    
+    //Agregar la etiqueta divisa 2
+    private void agregarEtiquetaDivisa2(){
+        jlDivisa2 = new JLabel("$");
+        jlDivisa2.setFont(new Font("Arial", 0, 36));
+        jlDivisa2.setHorizontalAlignment(SwingConstants.RIGHT);
+        
+        
+        c.gridx = 0;
+        c.gridy = 2;
+        c.gridwidth = 1;
+        c.gridheight = 1;
+        
+        c.fill = GridBagConstraints.BOTH;
+        c.anchor = GridBagConstraints.CENTER;
+        c.insets = new Insets(15, 15, 0, 5);
+        c.weightx = 0.5;
+        c.weighty = 0.5;
+        
+        
+        panel.add(jlDivisa2, c);
+    }
+
+    
+    //Agregar etiqueta cambio 2
+    private void agregarEtiquetaCambio2(){
+        jlCambio2 = new JLabel("0");
+        jlCambio2.setFont(new Font("Arial", 0, 36));
+        
+        c.gridx = 1;
+        c.gridy = 2;
+        c.gridwidth = 2;
+        c.gridheight = 1;
+        
+        c.fill = GridBagConstraints.BOTH;
+        c.anchor = GridBagConstraints.CENTER;
+        c.insets = new Insets(15, 0, 0, 0);
+        c.weightx = 0.5;
+        c.weighty = 0.5;
+        
+        panel.add(jlCambio2, c);
+    }
+    
+    //Agregar combo divisa 2
+    private void agregarComboDivisa2(){
+        comboDivisa2 = new JComboBox();
+        comboDivisa2.setFont(new Font("Arial", 0, 14));
+        
+        
+        c.gridx = 0;
+        c.gridy = 3;
+        c.gridwidth = 3;
+        c.gridheight = 1;
+        
+        c.fill = GridBagConstraints.BOTH;
+        c.anchor = GridBagConstraints.CENTER;
+        c.insets = new Insets(0, 10, 0, 10);
+        c.weightx = 0.5;
+        c.weighty = 0.5;
+        
+        
+        panel.add(comboDivisa2, c);
+        
+    }
+    
+    //Agregar botones
+    private void agregarBoton(){
+        
+        btnC = new JButton("C");
+        btnC.setFont(new Font("Arial", 0, 18));
+        
+        c.gridx = 1;
+        c.gridy = 4;
+        c.gridwidth = 1;
+        c.gridwidth = 1;
+        
+        c.fill = GridBagConstraints.BOTH;
+        c.anchor = GridBagConstraints.CENTER;
+        c.insets = new Insets(15, 0, 0, 0);
+        c.weightx = 0.5;
+        c.weighty = 0.5;
+        panel.add(btnC, c);
+        
+        
+        btnBorrar = new JButton("X");
+        btnBorrar.setFont(new Font("Arial", 0, 18));
+        
+        c.gridx = 2;
+        c.gridy = 4;
+        c.gridwidth = 1;
+        c.gridwidth = 1;
+        
+        c.fill = GridBagConstraints.BOTH;
+        c.anchor = GridBagConstraints.CENTER;
+        c.insets = new Insets(15, 0, 0, 0);
+        c.weightx = 0.5;
+        c.weighty = 0.5;
+        panel.add(btnBorrar, c);
+        
+        
+        btn7 = new JButton("7");
+        btn7.setFont(new Font("Arial", 0, 18));
+  
+        
+        c.gridx = 0;
+        c.gridy = 5;
+        c.gridwidth = 1;
+        c.gridwidth = 1;
+        
+        c.fill = GridBagConstraints.BOTH;
+        c.anchor = GridBagConstraints.CENTER;
+        c.weightx = 0.5;
+        c.weighty = 0.5;
+        panel.add(btn7, c);
+        
+        
+        btn8 = new JButton("8");
+        btn8.setFont(new Font("Arial", 0, 18));
+        btn8.setBackground(new Color(204, 204, 204));
+        
+        c.gridx = 1;
+        c.gridy = 5;
+        c.gridwidth = 1;
+        c.gridwidth = 1;
+        
+        c.fill = GridBagConstraints.BOTH;
+        c.anchor = GridBagConstraints.CENTER;
+        c.weightx = 0.5;
+        c.weighty = 0.5;
+        panel.add(btn8, c);
+        
+        
+        btn9 = new JButton("9");
+        btn9.setFont(new Font("Arial", 0, 18));
+        btn9.setBackground(new Color(204, 204, 204));
+        
+        c.gridx = 2;
+        c.gridy = 5;
+        c.gridwidth = 1;
+        c.gridwidth = 1;
+        
+        c.fill = GridBagConstraints.BOTH;
+        c.anchor = GridBagConstraints.CENTER;
+        c.weightx = 0.5;
+        c.weighty = 0.5;
+        panel.add(btn9, c);
+        
+        
+        btn4 = new JButton("4");
+        btn4.setFont(new Font("Arial", 0, 18));
+        btn4.setBackground(new Color(204, 204, 204));
+        
+        c.gridx = 0;
+        c.gridy = 6;
+        c.gridwidth = 1;
+        c.gridwidth = 1;
+        
+        c.fill = GridBagConstraints.BOTH;
+        c.anchor = GridBagConstraints.CENTER;
+        c.weightx = 0.5;
+        c.weighty = 0.5;
+        panel.add(btn4, c);
+        
+        btn5 = new JButton("5");
+        btn5.setFont(new Font("Arial", 0, 18));
+        btn5.setBackground(new Color(204, 204, 204));
+        
+       
+        c.gridx = 1;
+        c.gridy = 6;
+        c.gridwidth = 1;
+        c.gridwidth = 1;
+        
+        c.fill = GridBagConstraints.BOTH;
+        c.anchor = GridBagConstraints.CENTER;
+        c.weightx = 0.5;
+        c.weighty = 0.5;
+        panel.add(btn5, c);
+        
+        btn6 = new JButton("6");
+        btn6.setFont(new Font("Arial", 0, 18));
+        btn6.setBackground(new Color(204, 204, 204));
+        
+        c.gridx = 2;
+        c.gridy = 6;
+        c.gridwidth = 1;
+        c.gridwidth = 1;
+        
+        c.fill = GridBagConstraints.BOTH;
+        c.anchor = GridBagConstraints.CENTER;
+        c.weightx = 0.5;
+        c.weighty = 0.5;
+        panel.add(btn6, c);
+        
+       
+        btn1 = new JButton("1");
+        btn1.setFont(new Font("Arial", 0, 18));
+        btn1.setBackground(new Color(204, 204, 204));
+        
+        c.gridx = 0;
+        c.gridy = 7;
+        c.gridwidth = 1;
+        c.gridwidth = 1;
+        
+        c.fill = GridBagConstraints.BOTH;
+        c.anchor = GridBagConstraints.CENTER;
+        c.weightx = 0.5;
+        c.weighty = 0.5;
+        panel.add(btn1, c);
+        
+        
+        btn2 = new JButton("2");
+        btn2.setFont(new Font("Arial", 0, 18));
+        btn2.setBackground(new Color(204, 204, 204));
+        
+        c.gridx = 1;
+        c.gridy = 7;
+        c.gridwidth = 1;
+        c.gridwidth = 1;
+        
+        c.fill = GridBagConstraints.BOTH;
+        c.anchor = GridBagConstraints.CENTER;
+        c.weightx = 0.5;
+        c.weighty = 0.5;
+        panel.add(btn2, c);
+        
+         
+        
+        btn3 = new JButton("3");
+        btn3.setFont(new Font("Arial", 0, 18));
+        btn3.setBackground(new Color(204, 204, 204));
+        
+        c.gridx = 2;
+        c.gridy = 7;
+        c.gridwidth = 1;
+        c.gridwidth = 1;
+        
+        c.fill = GridBagConstraints.BOTH;
+        c.anchor = GridBagConstraints.CENTER;
+        c.weightx = 0.5;
+        c.weighty = 0.5;
+        panel.add(btn3, c);
+        
+
+        btn0 = new JButton("0");
+        btn0.setFont(new Font("Arial", 0, 18));
+        btn0.setBackground(new Color(204, 204, 204));
+        
+        c.gridx = 1;
+        c.gridy = 8;
+        c.gridwidth = 1;
+        c.gridwidth = 1;
+        
+        c.fill = GridBagConstraints.BOTH;
+        c.anchor = GridBagConstraints.CENTER;
+        c.weightx = 0.5;
+        c.weighty = 0.5;
+        panel.add(btn0, c);
+        
+        
+        btnPunto = new JButton(".");
+        btnPunto.setFont(new Font("Arial", 0, 18));
+        
+        c.gridx = 2;
+        c.gridy = 8;
+        c.gridwidth = 1;
+        c.gridwidth = 1;
+        
+        c.fill = GridBagConstraints.BOTH;
+        c.anchor = GridBagConstraints.CENTER;
+        c.weightx = 0.5;
+        c.weighty = 0.5;
+        panel.add(btnPunto, c);
+        
+
+    }
 }
